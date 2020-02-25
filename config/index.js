bc1982fa158ad4a2968288b9a08c5dd42c89f32a
michaelxu <michael.xu1983@qq.com>
2020-02-25 19:52:31 +0800

    fix(h5): 改为 browser,适应webview不支持井号

--- config/index.js
+++ config/index.js
@@ -1,59 +1,57 @@
 // eslint-disable-next-line import/no-commonjs
-const path = require('path')
+const path = require("path");
 
 const config = {
-  projectName: 'student-multi-web-client',
-  date: '2020-2-3',
+  projectName: "student-multi-web-client",
+  date: "2020-2-3",
   designWidth: 750,
   deviceRatio: {
-    '640': 2.34 / 2,
-    '750': 1,
-    '828': 1.81 / 2
+    "640": 2.34 / 2,
+    "750": 1,
+    "828": 1.81 / 2
   },
-  sourceRoot: 'src',
-  outputRoot: 'dist',
+  sourceRoot: "src",
+  outputRoot: "dist",
   babel: {
     sourceMap: true,
     presets: [
-      ['env', {
-        modules: false
-      }]
+      [
+        "env",
+        {
+          modules: false
+        }
+      ]
     ],
     plugins: [
-      'transform-decorators-legacy',
-      'transform-class-properties',
-      'transform-object-rest-spread',
-      ['transform-runtime', {
+      "transform-decorators-legacy",
+      "transform-class-properties",
+      "transform-object-rest-spread",
+      [
+        "transform-runtime",
+        {
           helpers: false,
           polyfill: false,
           regenerator: true,
-          moduleName: 'babel-runtime'
+          moduleName: "babel-runtime"
         }
       ]
     ]
   },
-  defineConstants: {
-  },
+  defineConstants: {},
   alias: {
-    '@/src': path.resolve(__dirname, '..', 'src'),
+    "@/src": path.resolve(__dirname, "..", "src")
   },
   mini: {
     postcss: {
       autoprefixer: {
         enable: true,
         config: {
-          browsers: [
-            'last 3 versions',
-            'Android >= 4.1',
-            'ios >= 8'
-          ]
+          browsers: ["last 3 versions", "Android >= 4.1", "ios >= 8"]
         }
       },
       pxtransform: {
         enable: true,
-        config: {
-
-        }
+        config: {}
       },
       url: {
         enable: true,
@@ -64,40 +62,39 @@ const config = {
       cssModules: {
         enable: true, // 默认为 false，如需使用 css modules 功能，则设为 true
         config: {
-          namingPattern: 'module', // 转换模式，取值为 global/module
-          generateScopedName: '[name]__[local]___[hash:base64:5]'
+          namingPattern: "module", // 转换模式，取值为 global/module
+          generateScopedName: "[name]__[local]___[hash:base64:5]"
         }
       }
     }
   },
   h5: {
-    publicPath: '/',
-    staticDirectory: 'static',
+    publicPath: "/",
+    staticDirectory: "static",
+    router: {
+      mode: "browser" // 或者是 'browser'
+    },
     postcss: {
       autoprefixer: {
         enable: true,
         config: {
-          browsers: [
-            'last 3 versions',
-            'Android >= 4.1',
-            'ios >= 8'
-          ]
+          browsers: ["last 3 versions", "Android >= 4.1", "ios >= 8"]
         }
       },
       cssModules: {
         enable: true, // 默认为 false，如需使用 css modules 功能，则设为 true
         config: {
-          namingPattern: 'module', // 转换模式，取值为 global/module
-          generateScopedName: '[name]__[local]___[hash:base64:5]'
+          namingPattern: "module", // 转换模式，取值为 global/module
+          generateScopedName: "[name]__[local]___[hash:base64:5]"
         }
       }
     }
   }
-}
+};
 
-module.exports = function (merge) {
-  if (process.env.NODE_ENV === 'development') {
-    return merge({}, config, require('./dev'))
+module.exports = function(merge) {
+  if (process.env.NODE_ENV === "development") {
+    return merge({}, config, require("./dev"));
   }
-  return merge({}, config, require('./prod'))
-}
+  return merge({}, config, require("./prod"));
+};
